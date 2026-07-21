249a1a3636cd6bbbd261d77af33e5cff1df75f3b
raouf1991 <raouf.tarek@gmail.com>
2026-07-21 14:49:46 +0300

    Load academy application upgrades

--- supabase-config.js
+++ supabase-config.js
@@ -3,7 +3,9 @@ window.KH_SUPABASE = {
   publishableKey: 'sb_publishable_nJDc_QBLF2IIr_we4PDJPQ_64YuLovu'
 };
 
-if (/\/admin(?:\.html)?$/.test(location.pathname) || location.pathname.endsWith('/admin.html')) {
+const isMainAdmin = /\/admin(?:\.html)?$/.test(location.pathname) || location.pathname.endsWith('/admin.html');
+
+if (isMainAdmin) {
   const approvalScript = document.createElement('script');
   approvalScript.src = 'admin-membership-approval.js?v=20260720-membership-card2';
   approvalScript.defer = true;
@@ -13,4 +15,14 @@ if (/\/admin(?:\.html)?$/.test(location.pathname) || location.pathname.endsWith(
   playerProfilesLinkScript.src = 'admin-player-profiles-link.js?v=20260721-1';
   playerProfilesLinkScript.defer = true;
   document.head.appendChild(playerProfilesLinkScript);
+
+  const academyAdminScript = document.createElement('script');
+  academyAdminScript.src = 'admin-academy.js?v=20260721-1';
+  academyAdminScript.defer = true;
+  document.head.appendChild(academyAdminScript);
+} else if (location.pathname.endsWith('/') || location.pathname.endsWith('/index.html')) {
+  const academyUpgradeScript = document.createElement('script');
+  academyUpgradeScript.src = 'academy-upgrade.js?v=20260721-1';
+  academyUpgradeScript.defer = true;
+  document.head.appendChild(academyUpgradeScript);
 }
