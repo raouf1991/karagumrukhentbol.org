a408e6dea23aee35b2a0a4a28532d0e310370406
raouf1991 <raouf.tarek@gmail.com>
2026-07-25 17:45:16 +0300

    Load final player-only homepage hero

--- supabase-config.js
+++ supabase-config.js
@@ -34,7 +34,7 @@ if (isMainAdmin) {
 
   if (location.pathname.endsWith('/') || location.pathname.endsWith('/index.html')) {
     const heroPlayerScript = document.createElement('script');
-    heroPlayerScript.src = 'hero-player-loader.js?v=20260725-4';
+    heroPlayerScript.src = 'hero-player-loader.js?v=20260725-final1';
     heroPlayerScript.defer = true;
     document.head.appendChild(heroPlayerScript);
 
