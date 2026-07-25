4a8cfb9176aa4865e7f6769c8a789514203d00d4
raouf1991 <raouf.tarek@gmail.com>
2026-07-25 16:29:18 +0300

    Bust cache for corrected homepage hero loader

--- supabase-config.js
+++ supabase-config.js
@@ -34,7 +34,7 @@ if (isMainAdmin) {
 
   if (location.pathname.endsWith('/') || location.pathname.endsWith('/index.html')) {
     const heroPlayerScript = document.createElement('script');
-    heroPlayerScript.src = 'hero-player-loader.js?v=20260725-1';
+    heroPlayerScript.src = 'hero-player-loader.js?v=20260725-4';
     heroPlayerScript.defer = true;
     document.head.appendChild(heroPlayerScript);
 
