d406a5fdadbe1ec652c4b7222de27c1915f149ba
raouf1991 <raouf.tarek@gmail.com>
2026-07-26 14:24:01 +0300

    Force fresh contact cleanup script

--- supabase-config.js
+++ supabase-config.js
@@ -39,7 +39,7 @@ if (isMainAdmin) {
     document.head.appendChild(heroPlayerScript);
 
     const contactUpgradeScript = document.createElement('script');
-    contactUpgradeScript.src = 'contact-upgrade.js?v=20260725-2';
+    contactUpgradeScript.src = 'contact-upgrade.js?v=20260726-3';
     contactUpgradeScript.defer = true;
     document.head.appendChild(contactUpgradeScript);
 
